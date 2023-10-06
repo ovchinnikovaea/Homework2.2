b2d712d5aca62c58f8919816c2c4997905549f28
ovchinnikova <ov4innikova.e.a@mail.ru>
2023-10-06 19:17:03 +0300

    исправила замечанияя

--- src/Hogwarts.java
+++ src/Hogwarts.java
@@ -1,10 +1,10 @@
-public class hogwarts {
+public abstract class Hogwarts {
 
     private String fullName;
     private int powerOfMagic; //сила магии
     private int transgressionDistance; //расстояние трансгрессии
 
-    public hogwarts(String fullName, int powerOfMagic, int transgressionDistance) {
+    public Hogwarts(String fullName, int powerOfMagic, int transgressionDistance) {
         this.fullName = fullName;
         this.powerOfMagic = powerOfMagic;
         this.transgressionDistance = transgressionDistance;
@@ -34,9 +34,9 @@ public class hogwarts {
         this.transgressionDistance = transgressionDistance;
     }
 
-    public void printStudents(hogwarts[] students) {
+    public void printStudents(Hogwarts[] students) {
         for (int i = 0; i < students.length; i++) {
-            hogwarts faculty = students[i];
+            Hogwarts faculty = students[i];
             System.out.println("Имя " + faculty.getFullName()
                     + " Сила магии " + faculty.getPowerOfMagic()
                     + " Расстояние трансгрессии " + faculty.getTransgressionDistance());
@@ -44,7 +44,7 @@ public class hogwarts {
         }
     }
 
-    public void compareAnyStudents(hogwarts student) {
+    public void compareAnyStudents(Hogwarts student) {
         if (this.getPowerOfMagic() > student.getPowerOfMagic()) {
             System.out.println(this.getFullName() + " обладает большей силой магии, чем " + student.getFullName());
         } else {
